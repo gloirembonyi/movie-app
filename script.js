2bfd127814b0c5bacebe4776cd454fc5fbf46835
Gloire Mbonyimana <123030808+gloirembonyi@users.noreply.github.com>
2023-12-09 11:51:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+
+function buttonSub(){
+    const buttonElement = document.querySelector('.button-subscribe');
+
+    if (buttonElement.innerText ==='Subscribe') {
+       buttonElement.innerHTML = 'Subscribed';
+       buttonElement.classList.add('submit-button');
+    }else {
+       buttonElement.innerHTML = 'Subscribe';
+       buttonElement.classList.remove('submit-button');
+
+    }
+ };
+
+
+ const swiper = new Swiper('.swiper', {
+    autoplay:{
+       delay: 3000,
+       disableOnInteraction: false,
+    },
+// Optional parameters
+loop: true,
+
+// If we need pagination
+pagination: {
+ el: '.swiper-pagination',
+ clickable:true,
+},
+
+// Navigation arrows
+navigation: {
+ nextEl: '.swiper-button-next',
+ prevEl: '.swiper-button-prev',
+},
+
+ });
